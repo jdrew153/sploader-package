26c327f85c21099109096bc708b496bec13b203c
joshuadrew <jdrew153@gmail.com>
2023-07-28 15:55:18 -0400

    updates

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,13 @@
 import { TSploaderUploadHookRequest } from './path/to/TSploaderUploadHookRequest'; // Adjust the import path as per your actual file structure
 
-export declare function useKaykatJDUploader(): {
+declare const useKaykatJDUploader: () => {
     progress: number;
     fileUrl: string | null;
     isLoading: boolean;
     error: unknown | null;
     uploadFile: (data: TSploaderUploadHookRequest) => Promise<void>;
 };
+
+
+export default useKaykatJDUploader;
+
